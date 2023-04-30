f6577a4ad62ae0d4aa80e6948b30bf679db87366
piece-of-bit <alex.gortik@gmail.com>
2023-04-30 21:06:18 +0500

    ну куки и хеш есть

--- reg.php
+++ reg.php
@@ -1,18 +1,15 @@
 <?php
 require_once "db.php";
-if(!empty($_POST['login']))
-{
-  $fio=$_POST['fio'];
-  $f_i_o = explode(" ", $fio);
-  $stmt = $pdo->prepare("insert into user(userlogin, password, firstname, lastname, patronymic, email) values(?,?,?,?,?,?)"); 
-  $stmt->execute([
-    $_POST['login'], 
-    $_POST['pass'], 
-    $f_i_o[0], 
-    $f_i_o[1], 
-    $f_i_o[2], 
-    $_POST['mail']
-  ]); 
-  header("Location: index.html");
-} 
+$fio=$_POST['fio'];
+$f_i_o = explode(" ", $fio);
+$stmt = $pdo->prepare("insert into user(userlogin, password, lastname, firstname, patronymic, email) values(?,?,?,?,?,?)"); 
+$stmt->execute([
+  $_POST['login'], 
+  hash("sha256", $_POST['pass'], false), 
+  $f_i_o[0], 
+  $f_i_o[1], 
+  $f_i_o[2], 
+  $_POST['mail']
+]); 
+header("Location: index.html");
 ?>
